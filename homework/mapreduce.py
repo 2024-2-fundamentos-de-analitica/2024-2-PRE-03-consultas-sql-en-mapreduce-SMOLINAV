d2d9d3f909430106e9c0b1cb9c8db1d4422a476d
SMOLINAV <111089184+SMOLINAV@users.noreply.github.com>
2024-11-13 11:41:27 -0500

    intento

--- homework/mapreduce.py
+++ homework/mapreduce.py
@@ -6,9 +6,6 @@ import fileinput
 import glob
 import os.path
 
-
-
-
 def _load_input(input_directory):
     sequence = []
     files = glob.glob(f"{input_directory}/*")
@@ -16,11 +13,9 @@ def _load_input(input_directory):
         for line in f:
             sequence.append((fileinput.filename(), line))
     return sequence
-
-
+  
 def _shuffle_and_sort(sequence):
     return sorted(sequence, key=lambda x: x[0])
-
 
 def _create_ouptput_directory(output_directory):
     if os.path.exists(output_directory):
@@ -28,13 +23,11 @@ def _create_ouptput_directory(output_directory):
             os.remove(file)
         os.rmdir(output_directory)
     os.makedirs(output_directory)
-
-
+    
 def _save_output(output_directory, sequence):
     with open(f"{output_directory}/part-00000", "w", encoding="utf-8") as f:
         for key, value in sequence:
             f.write(f"{key}\t{value}\n")
-
 
 def _create_marker(output_directory):
     """Create Marker"""
@@ -51,5 +44,3 @@ def run_mapreduce_job(mapper, reducer, input_directory, output_directory):
     _create_ouptput_directory(output_directory)
     _save_output(output_directory, sequence)
     _create_marker(output_directory)
-
-
